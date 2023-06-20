42d2e02c1c8b3350a12cf82411cea0f7c6b60e19
marcusccl16 <s10219337@connect.np.edu.sg>
2023-06-20 23:17:58 +0800

    added login page

---
+++ app/src/main/java/com/example/madasg1iman/LoginActivity.java
@@ -0,0 +1,49 @@
+package com.example.madasg1iman;
+
+import static android.provider.MediaStore.MediaColumns.TITLE;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailinput, passwordinput;
+    private Button loginbutton, registerbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v(TITLE, "CREATE!");
+        initView();
+        setVariable();
+    }
+    private void setVariable(){
+        loginbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(!emailinput.getText().toString().isEmpty() && !passwordinput.getText().toString().isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Please fill in your particulars.", Toast.LENGTH_SHORT).show();
+                }
+                else if(emailinput.getText().toString().equals("madassg1") && passwordinput.getText().toString().equals("madassg1"))
+                {
+                    startActivity(new Intent(LoginActivity.this,HomePage.class));
+                }
+            }
+        });
+    }
+
+    private void initView(){
+        emailinput = findViewById(R.id.enteremail);
+        passwordinput = findViewById(R.id.enterpassword);
+        loginbutton = findViewById(R.id.login);
+        registerbutton = findViewById(R.id.register);
+    }
+}
--- app/src/main/java/com/example/madasg1iman/Splashscreen.java
+++ app/src/main/java/com/example/madasg1iman/Splashscreen.java
@@ -15,7 +15,7 @@ public class Splashscreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splashscreen.this, HomePage.class);
+                Intent intent = new Intent(Splashscreen.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
