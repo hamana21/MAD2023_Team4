5fa5bc9311c70e598d0cd7da5744d1bcc96c7a87
apple <aarafrao22@gmail.com>
2023-07-28 09:11:32 +0500

    Contact Us design

---
+++ app/src/main/java/sg/np/madasg1iman/ContactUsActivity.java
@@ -0,0 +1,16 @@
+package sg.np.madasg1iman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.madasg1iman.R;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+    }
+}
