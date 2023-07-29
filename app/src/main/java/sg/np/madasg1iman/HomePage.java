72276ef4e66b1e9438f6f27043dec507bae47b7e
Muhd Nuriman <59644939+hamana21@users.noreply.github.com>
2023-07-30 04:42:32 +0800

    added home, about, and contact fragment page
    asdadasdasdas

--- app/src/main/java/sg/np/madasg1iman/HomePage.java
+++ app/src/main/java/sg/np/madasg1iman/HomePage.java
@@ -1,22 +1,31 @@
 package sg.np.madasg1iman;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.madasg1iman.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 
 public class HomePage extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
 
-    private ImageButton papers_btn, online_btn, account_btn, about_btn, imageButton, btmAboutButton, btmAccountBtn;
+    HomeFragment homeFragment = new HomeFragment();
+    AboutFragment aboutFragment = new AboutFragment();
+    AccountFragment accountFragment = new AccountFragment();
+
+    private ImageButton papers_btn, online_btn, account_btn, about_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +34,31 @@ public class HomePage extends AppCompatActivity {
         online_btn = findViewById(R.id.imageButton7);
         account_btn = findViewById(R.id.imageButton4);
         about_btn = findViewById(R.id.imageButton5);
-        imageButton = findViewById(R.id.imageButton);
-        btmAboutButton = findViewById(R.id.btmAboutImg);
-        btmAccountBtn = findViewById(R.id.btmAccountBtn);
         //logoutbtn = findViewById(R.id.logout)
+
+
         buttonEvents();
+
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.home) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                } else if (id == R.id.info) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, aboutFragment).commit();
+                } else if (id == R.id.account) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, accountFragment).commit();
+                }
+                return true;
+            }
+        });
     }
     private void buttonEvents(){
 
@@ -57,54 +86,8 @@ public class HomePage extends AppCompatActivity {
         account_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(HomePage.this, HomePage.class));
+                startActivity(new Intent(HomePage.this, ContactUsActivity.class));
             }
         });
-
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Clear all activities
-                Intent intent = new Intent(HomePage.this, HomePage.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }
-        });
-
-        btmAboutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Clear all activities
-                Intent intent = new Intent(HomePage.this, AboutMe.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }
-        });
-
-        btmAccountBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Clear all activities
-                Intent intent = new Intent(HomePage.this, HomePage.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-            }
-        });
-
-
-        btmAccountBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Clear all activities
-                Intent intent = new Intent(HomePage.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-                Toast.makeText(HomePage.this, "You have logged out!",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-
     }
 }
