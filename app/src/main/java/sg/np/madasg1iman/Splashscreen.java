94a3e9b21be1326bb7d4c0e2a7ec965ec1cfaaaf
Muhd Nuriman <59644939+hamana21@users.noreply.github.com>
2023-07-31 08:14:13 +0800

    WORKING without login and register
    issues with login?

--- app/src/main/java/sg/np/madasg1iman/Splashscreen.java
+++ app/src/main/java/sg/np/madasg1iman/Splashscreen.java
@@ -17,7 +17,7 @@ public class Splashscreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splashscreen.this, LoginActivity.class);
+                Intent intent = new Intent(Splashscreen.this, HomePage.class);
                 startActivity(intent);
                 finish();
             }
